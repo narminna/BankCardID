a8d1f36a91b49515ca9c6505bb8cd2133e683296
narminna <135731180+narminna@users.noreply.github.com>
2023-06-22 17:09:23 +0400

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,7 +1,19 @@
 ﻿using System;
+
 namespace BankCard
 {
-    public class BankCard
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BankCard bankCard = new BankCard("1234 5678 9012 3456", "123", "12/26", "Cooper Joseph", "ABB", 5000m);
+            bankCard.Car();
+
+            Console.ReadKey();
+        }
+    }
+
+    class BankCard
     {
         public string CardNumber { get; set; }
         public string CVV { get; set; }
@@ -9,21 +21,20 @@ namespace BankCard
         public string CardholderName { get; set; }
         public string BankName { get; set; }
         public decimal Balance { get; set; }
-    }
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            BankCard myCard = new BankCard();
 
-            myCard.CardNumber = "1234 5678 9123 4567";
-            myCard.CVV = "123";
-            myCard.ExpiryDate = "12/26";
-            myCard.CardholderName = "Cooper Joseph";
-            myCard.BankName = "ABB";
-            myCard.Balance = 5000;
+        public BankCard(string cardNumber, string cvv, string expiryDate, string cardholderName, string bankName, decimal balance)
+        {
+            this.CardNumber = cardNumber;
+            this.CVV = cvv;
+            this.ExpiryDate = expiryDate;
+            this.CardholderName = cardholderName;
+            this.BankName = bankName;
+            this.Balance = balance;
+        }
+
+        public void Car()
+        {
+            Console.WriteLine("Bank Card Data: " + CardNumber + " " + CVV + " " + ExpiryDate + " " + CardholderName + " " + BankName + " " + Balance);
         }
     }
 }
-
-
